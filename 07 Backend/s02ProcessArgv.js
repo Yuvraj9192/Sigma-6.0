50009f5ef9cb75afca79a99d2493ebec4265368a
Yuvraj9192 <yuvraj.shr07@gmail.com>
2025-03-15 13:21:28 +0530

    s02ProcessArgv.js added

---
+++ 07 Backend/s02ProcessArgv.js
@@ -0,0 +1,5 @@
+console.log(process);
+console.log("************************************");
+console.log("value of process.cwd(): ",process.cwd());
+console.log("************************************");
+console.log("value of process.argv: ",process.argv);
