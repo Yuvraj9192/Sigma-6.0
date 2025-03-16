f62dd01c9798ff278f2b22e477a80f72777e0e59
Yuvraj9192 <yuvraj.shr07@gmail.com>
2025-03-16 13:16:19 +0530

    added folders

---
+++ 07 Backend/linkings04Import.js
@@ -0,0 +1,3 @@
+import {sum,mul,g, pie} from "./s04Import.js";
+
+console.log("answer: ", mul(5,2));
---
+++ RandomWord.js
@@ -0,0 +1,5 @@
+import { generate, count } from "random-words";
+
+console.log(generate());
+console.log(generate(5));
+
