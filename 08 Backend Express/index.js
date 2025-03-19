855ffa65f17afec36c32ff4a92cbb262f756a021
Yuvraj9192 <yuvraj.shr07@gmail.com>
2025-03-19 17:57:16 +0530

    EXPRESS DONE

--- 08 Backend Express/index.js
+++ 08 Backend Express/index.js
@@ -1,9 +1,16 @@
+// handling the request
+// sending the responses
+// routing
+// working of nodemon to automatically detect the change and restart the server.
+// path parameters
+// Query Strings
+
 const express = require("express");
 const app = express();
 
 console.dir(app);
 
-let port = 3005;
+let port = 8080;
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
@@ -26,11 +33,11 @@ app.get("/", (req, res) => {
     res.send(code);
 });
 
-app.get("/search", (req, res) => {
-    console.log("Request received");
-    let code = `<input type="text" placeholder="search">`;
-    res.send(code);
-});
+// app.get("/search", (req, res) => {
+//     console.log("Request received");
+//     let code = `<input type="text" placeholder="search">`;
+//     res.send(code);
+// });
 
 app.get("/help", (req, res) => {
     console.log("Request received");
@@ -40,6 +47,32 @@ app.get("/help", (req, res) => {
     res.send(code);
 });
 
+
+// path parameters
+app.get("/:username/:id", (req, res) => {
+    console.log(req.params);
+    const { username, id } = req.params;
+    res.send(`hello, this profile belongs to ${username} with id pass ${id}`);
+});
+
+// request parameters
+app.get("/search", (req, res) => {
+    const { q } = req.query;
+    if (!q) {
+        return res.status(400).send("Please provide a search query.");
+    }
+    res.send(`Search result for query: ${q}`);
+});
+
+
+
+
+
+
+
+
+
+// if anything doesnot works then show this 
 app.get("*", (req, res) => {
     res.send("this path does not exits :(");
 });
