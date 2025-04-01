e8a173d313c2233701873c72683657d04270c7d2
Yuvraj9192 <yuvraj.shr07@gmail.com>
2025-04-02 01:40:08 +0530

    all done

--- 11 Quora REST/index.js
+++ 11 Quora REST/index.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 8080;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
+
 
 
 app.use(express.urlencoded({extended: true}));
-
+app.use(methodOverride('_method'))
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -37,7 +39,8 @@ app.get("/posts", (req, res) => {
 app.post("/post", (req,res) => {
     // console.log(req.body);
     let {username, content} = req.body;
-    posts.push({username, content});
+    id = uuidv4();
+    posts.push({id,username, content});
     // res.send("post request working! yeah");
     res.redirect("/posts");
 });
@@ -48,6 +51,7 @@ app.get("/posts/new", (req,res) =>{
 
 app.get("/posts/:id", (req, res) => {
     let {id} = req.params;
+    console.log(id);
     let post = posts.find((p) => id === p.id);
     console.log(post);
     // res.send("request is working!!");
@@ -55,8 +59,33 @@ app.get("/posts/:id", (req, res) => {
 });
 
 // app.get("/post/new", (req, res) => {
-//     res.render();
-// });
+    //     res.render();
+    // });
+    
+app.patch("/posts/:id", (req,res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    // console.log(newContent);
+    // console.log(id);
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    // res.send("patch is working yeah!!");
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    // res.send("deleted success");
+    res.redirect("/posts");
+});
 
 app.listen(port, ()=> {
     console.log(`listening to port : ${port}`);
