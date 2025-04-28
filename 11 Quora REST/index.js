edbd22a9592b2cd47d0a59c4d70effc555e36104
Yuvraj9192 <yuvraj.shr07@gmail.com>
2025-04-29 02:11:56 +0530

    updated till quora

--- 11 Quora REST/index.js
+++ 11 Quora REST/index.js
@@ -6,9 +6,9 @@ const { v4: uuidv4 } = require('uuid');
 const methodOverride = require("method-override");
 
 
-
+//app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
